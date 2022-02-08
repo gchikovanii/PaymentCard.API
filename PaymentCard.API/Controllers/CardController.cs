5eb7405013216869ae86956e717d12a5eead65b8
chikovani2001 <chikovani2001@inbox.ru>
2022-02-08 16:00:49 +0400

    First Version of Card.API

---
+++ PaymentCard.API/Controllers/CardController.cs
@@ -0,0 +1,55 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PaymentCard.Application.Command.CardAggregate;
+using PaymentCard.Application.Query;
+using PaymentCard.Application.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace PaymentCard.API.Controllers
+{
+    public class CardController: BaseController
+    {
+        private readonly IMediator _mediator;
+        private readonly ICardService _cardService;
+        public CardController(IMediator mediator, ICardService cardService)
+        {
+            _mediator = mediator;
+            _cardService = cardService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllCards()
+        {
+            var cards = await _mediator.Send(new GetCardQuery());
+            return Ok(cards);
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddCard([FromBody] CreateCardCommand input)
+        {
+            return Ok(await _mediator.Send(input));
+        }
+        [HttpPut]
+        public async Task<IActionResult> UpdateCard(UpdateCardCommand input)
+        {
+            return Ok(await _mediator.Send(input));
+        }
+        [HttpDelete]
+
+        public async Task<IActionResult> DeleteCard(DeleteCardCommand input)
+        {
+            return Ok(await _mediator.Send(input));
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetHotelById(int id)
+        {
+            var hotel = await _cardService.GetCardsById(id);
+            return Ok(hotel);
+        }
+
+
+    }
+}
